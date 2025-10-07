d9beb72128289e9436c431f65801648927235073
LorenzoBruscato <lorenzobruscato6@gmail.com>
2025-10-07 14:29:44 -0300

    Initial Commit

---
+++ src/main/java/principal/Principal.java
@@ -0,0 +1,28 @@
+package principal;
+
+import java.net.*;
+import java.io.*;
+import javax.swing.*;
+
+public class Principal {
+
+    public static void main(String args[]) {
+        String ip = JOptionPane.showInputDialog("");
+        try {
+            DatagramSocket con = new DatagramSocket();//cria o objeto
+            System.out.println("Inicio cliente");
+            String mensagem = "";
+            while (!mensagem.equals("fim")) {
+                mensagem = JOptionPane.showInputDialog("Digite a Mensagem ou fim para sair");
+                //Converte a mensagem em um vetor de byte
+                byte dado[] = mensagem.getBytes();
+                //Configura o pacote
+                DatagramPacket pacote = new DatagramPacket(dado,
+                        dado.length, InetAddress.getByName(ip), 4444);
+                con.send(pacote); //envia o pacote
+            }
+        } catch (IOException io) {
+            System.err.println("Problemas de IO");
+        }
+    }
+}
